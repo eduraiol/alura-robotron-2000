51b039fe3ea7bf9079bd38b77baa05375adc112a
Eduardo Raiol <marquesraiol@gmail.com>
2023-01-28 08:18:44 -0300

    projeto final do Robotron JS1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const controle = document.querySelectorAll('[data-controle]');
-const estatisticas = document.querySelectorAll('[data-estatistica');
+const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
 
 const pecas = {
     "bracos": {
@@ -37,24 +37,23 @@ const pecas = {
 
 controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
-       manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
-       atualizaEstatisticas(evento.target.dataset.peca);
-    });
-});
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        atualizaEstatistica(evento.target.dataset.peca)
+    })
+})
 
 function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector('[data-contador]');
-    
-    if(operacao === "-"){
-        peca.value = parseInt(peca.value) - 1;
-    }else {
-        peca.value = parseInt(peca.value) + 1;
+    const peca = controle.querySelector("[data-contador]")
+
+    if(operacao === "-") {
+        peca.value = parseInd(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
     }
 }
 
-
-function atualizaEstatisticas (peca){
-    estatisticas.forEach( (elemento) => {
-        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
-    });
+function atualizaEstatistica(peca) {
+    estatistica.forEach( (elemento ) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
 }
